f520db188ad35dd382aa602e64170a77dda471fe
hongming.low <hongming.low@atoz-software.tech>
2025-10-26 03:34:47 +0800

    feat: added search screen and cart commponents

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
+import { images } from "@/constants";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="h-full flex-center">
+      <Text className="text-center text-dark-100 font-quicksand-semibold text-lg">
+        Profile
+      </Text>
+      <View className="rounded-full size-24 bg-gray-50 mt-9 flex-center">
+        <Image source={images.user} className="size-16" />
+      </View>
+    </SafeAreaView>
   );
 };
 
